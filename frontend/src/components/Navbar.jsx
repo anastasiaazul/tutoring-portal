173f8328fa802f5a94a6626d78d049c05a36c649
anastasiaazul <anastasia.k.odonnell@gmail.com>
2025-08-19 10:35:26 +0100

    add new style to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,18 +4,16 @@ import Logo from './Logo'
 
 const Navbar = () => {
   return (
-     
-
-      <div className="bg-sky-900 h-[10vh] flex items-center border-b-2 border-b-black justify-between m-0 p-0">
-        <div className="flex items-center">
-             <img src="./assets/symbols2.png" className="inline mx-2"/>
-            <p className="font-anton text-2xl font-extrabold">Portal</p>
-        </div>
-        <div className="flex gap-[2px]">
-          <Link><button className="px-[20px] py-[5px]    rounded-full hover:bg-sky-400 ">About</button></Link>
-          <Link to="/login" > <button className="px-[20px] py-[5px] bg-gray-950  mx-4 rounded-full hover:bg-sky-400 text-white">Log In</button></Link>
-        </div>
-    </div>
+    <nav className="bg-sky-900 h-[10vh] flex items-center border-b-2 border-b-black justify-between px-8 overflow-visible">
+      <Logo />
+      <div className="flex items-center">
+        <Link to="/login">
+          <button className="px-4 py-1 bg-white text-sky-900 font-semibold rounded shadow hover:bg-sky-100 transition text-base">
+            Log In
+          </button>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
