9b710ab63b5315a0dc88f113793c89e49ddc8af7
anastasiaazul <anastasia.k.odonnell@gmail.com>
2025-08-19 10:36:01 +0100

    added new services to connect to API

---
+++ frontend/src/services/login.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+
+const login = async credentials => {
+    const response =  await axios.post('http://localhost:3001/api/login', credentials)
+    return response.data;
+}
+
+export default {login}
